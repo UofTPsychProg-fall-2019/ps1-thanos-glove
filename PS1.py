e0abb360485cc3c7594a2edb2d143f25ea4e7143
Ben <benedict.park@mail.utoronto.ca>
2019-09-22 17:28:59 -0400

    BennyP_Final_Submission
    Hyun Goo Park's Final Submission for PS1.

--- PS1.py
+++ PS1.py
@@ -5,9 +5,9 @@ Problem Set 1
 @author: katherineduncan
 """
 
-#%% This is the final submission for Joseph Saito. The rest of the group (Tiana 
-#Simovic, Benedict Park) will push a final submission for the whole group when 
-#everyone finishes
+#%% This is the final submission for Ben. The rest of the group (Tiana 
+#Simovic) will push a final submission for the whole group when 
+#everyone finishes.
 
 #%% Part 1: pass the error forward ____________________________________________
 # this should be completed one at a time to get practice using GitHub
@@ -40,26 +40,38 @@ rt = [400, 450, 500, 440, -1, 410, 570]
 
 # the -1 indicates missing data. Your job is to remove it
 # use the index method to find the missing value 
-missing_rt =
+missing_rt = rt.index(-1)
 
 # and then use missing_rt to remove the trial from rt
-clean_rt =
-
+del(rt[missing_rt])
+clean_rt = rt;
 
 # now you have data with more than one missing value
 rt_trouble = [400, 450, 500, 440, -1, 410, 570, -1, 400]
 
 # try the same procedure. Does it work? 
 # use a comment to explain why or why not below in comments
-
-
+missing_rt = rt_trouble.index(-1)
+del(rt_trouble[missing_rt])
+clean_rt = rt; #not actually clean
+# BEN: This does not work for the second '-1' in the list because 
+# rt_trouble.index returns the position of only the first '-1' in the list.
 
 # now write an if statement that you can use to remove the frist missing value 
 # only when there is a missing value (-1) in a list 
 # this statement should always generate a clean_rt list; if there's no missing
 # data clean_rt is set to the original rt list.   
 
+rt_trouble = [400, 450, 500, 440, -1, 410, 570, -1, 400]
 
+if (-1 in rt_trouble):
+    missing_rt = rt_trouble.index(-1)
+    del(rt_trouble[missing_rt])
+    print('rt_trouble was not clean')
+else:
+    print('rt_trouble was clean')
+    
+clean_rt = rt_trouble
 
 # for the last section, you will work with a list of lists:
 rt_new = [400, 450, 500, 440, -1, 410, 570]
@@ -73,4 +85,15 @@ data = [rt_new, trial_num, accuracy]
 # and remove it from all sublists in data 
 # be sure to only work with the master data list, to practice indexing 
 # lists of lists
+
+if (-1 in rt_new):
+    missing_rt = rt_new.index(-1)
+    del(data[0][missing_rt])
+    del(data[1][missing_rt])
+    del(data[2][missing_rt])
+    print('rt_new was not clean')
+else:
+    print('rt_new was clean')
+    
+clean_data = data
 
